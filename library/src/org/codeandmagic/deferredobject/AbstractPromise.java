734857f388a7f037a5e96800fc5f6f6a7d0b202f
Cristian Vrabie <cristian.vrabie@gmail.com>
2012-07-10 21:42:24 +0100

    Added Copyright

--- library/src/org/codeandmagic/deferredobject/AbstractPromise.java
+++ library/src/org/codeandmagic/deferredobject/AbstractPromise.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2012 Cristian Vrabie, Evelina Vrabie
+ *
+ * This file is part of android-deferred-object.
+ *
+ * android-deferred-object is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * android-deferred-object is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.codeandmagic.deferredobject;
 
 import org.codeandmagic.deferredobject.pipe.*;
