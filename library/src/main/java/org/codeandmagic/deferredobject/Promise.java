91ee2a232893c0bb2b7adb28f1cfae50e6bfa939
Evelina Vrabie <evelyne24@gmail.com>
2014-02-09 20:38:25 +0000

    Build library and sample with Gradle

---
+++ library/src/main/java/org/codeandmagic/deferredobject/Promise.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2012 Cristian Vrabie, Evelina Vrabie
+ *
+ * This file is part of android-deferred-object.
+ *
+ * android-deferred-object is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * android-deferred-object is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.codeandmagic.deferredobject;
+
+import org.codeandmagic.deferredobject.pipe.ProgressFilter;
+import org.codeandmagic.deferredobject.pipe.RejectFilter;
+import org.codeandmagic.deferredobject.pipe.ResolveFilter;
+import org.codeandmagic.deferredobject.pipe.ResolvePipe;
+
+/** User: cvrabie1 Date: 09/07/2012 */
+public interface Promise<Resolved, Rejected, Progress> {
+
+  public enum State {
+    PENDING, REJECTED, RESOLVED
+  }
+
+  public State state();
+  public boolean isPending();
+  public boolean isRejected();
+  public boolean isResolved();
+
+  public Promise<Resolved, Rejected, Progress> then(final ResolveCallback<Resolved> onResolve);
+
+  public Promise<Resolved, Rejected, Progress> then(final ResolveCallback<Resolved> onResolve, final RejectCallback<Rejected> onReject);
+
+  public Promise<Resolved, Rejected, Progress> then(final ResolveCallback<Resolved> onResolve, final RejectCallback<Rejected> onReject, final ProgressCallback<Progress> onProgress);
+
+  public Promise<Resolved, Rejected, Progress> then(final ResolveCallback<Resolved> onResolve, final RejectCallback<Rejected> onReject, final ProgressCallback<Progress> onProgress, final CompleteCallback<Resolved, Rejected> onComplete);
+
+  public Promise<Resolved, Rejected, Progress> always(final CompleteCallback<Resolved, Rejected> onComplete);
+
+  public Promise<Resolved, Rejected, Progress> done(final ResolveCallback<Resolved> onResolve);
+
+  public Promise<Resolved, Rejected, Progress> fail(final RejectCallback<Rejected> onReject);
+
+  public Promise<Resolved, Rejected, Progress> progress(final ProgressCallback<Progress> onProgress);
+
+
+  public <Resolved2, Rejected2, Progress2> Promise<Resolved2, Rejected2, Progress2>
+         pipe(final ResolveFilter<Resolved, Resolved2> resolveFilter,
+			  final RejectFilter<Rejected, Rejected2> rejectFilter,
+			  final ProgressFilter<Progress, Progress2> progressFilter);
+
+  public <Resolved2, Rejected2, Progress2> Promise<Resolved2, Rejected2, Progress2>
+          pipe(final ResolvePipe<Resolved, Resolved2, Rejected2, Progress2> resolvedPipe);
+
+  public  <Resolved2> Promise<Resolved2, Rejected, Progress>
+          pipe(final ResolveFilter<Resolved, Resolved2> resolveFilter);
+
+
+
+}
+
---
+++ sample/src/main/java/org/codeandmagic/deferredobject/MainActivity.java
@@ -0,0 +1,74 @@
+package org.codeandmagic.deferredobject;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+
+import org.codeandmagic.deferredobject.android.DeferredHttpUrlConnection;
+import org.codeandmagic.deferredobject.merge.MergedPromiseReject;
+import org.codeandmagic.deferredobject.merge.MergedPromiseResult2;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends ActionBarActivity implements
+		ResolveCallback<HttpURLConnection>, RejectCallback<HttpURLConnection> {
+
+	private static final String TAG = "Test";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		setup();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	private void setup() {
+		try {
+			Promise<HttpURLConnection, HttpURLConnection, Void> p1 = new DeferredHttpUrlConnection(
+					new URL("http://www.google.com"));
+
+			p1.done(this).fail(this);
+
+			DeferredObject
+					.when(p1, new DeferredHttpUrlConnection(new URL("http://1")))
+					.done(new ResolveCallback<MergedPromiseResult2<HttpURLConnection, HttpURLConnection>>() {
+						@Override
+						public void onResolve(
+								MergedPromiseResult2<HttpURLConnection, HttpURLConnection> resolved) {
+							Log.i(TAG, "Done for both!" + resolved._1 + ", " + resolved._2);
+						}
+					}).fail(new RejectCallback<MergedPromiseReject>() {
+
+				@Override
+				public void onReject(MergedPromiseReject rejected) {
+					Log.e(TAG, "One failed: " + rejected.rejected + " index is "
+							+ rejected.index);
+				}
+			});
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onResolve(HttpURLConnection resolved) {
+		Log.d(TAG, "Done: " + resolved);
+	}
+
+	@Override
+	public void onReject(HttpURLConnection rejected) {
+		Log.w(TAG, "Fail: " + rejected);
+
+	}
+}
