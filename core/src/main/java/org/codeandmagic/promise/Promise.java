c340724d5a3879654258913aaa996696b7f69f6e
Evelina Vrabie <evelyne24@gmail.com>
2014-02-28 22:31:31 +0000

    Update copyright.

--- core/src/main/java/org/codeandmagic/promise/Promise.java
+++ core/src/main/java/org/codeandmagic/promise/Promise.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2014 Cristian Vrabie, Evelina Vrabie.
+ * Copyright (c) 2014 CodeAndMagic
+ * Cristian Vrabie, Evelina Vrabie
  *
  * This file is part of android-promise.
  * android-promise is free software: you can redistribute it and/or modify it
@@ -8,12 +9,12 @@
  * any later version.
  *
  * android-promise is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with android-promise
- * If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with android-promise. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.codeandmagic.promise;
 
